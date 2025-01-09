f97a974ddc9d953ae49365861e4bc72e387608e5
Nitesh Sharma <niteshsharma29.ns@gmail.com>
2025-01-09 20:05:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # these three lines swap the stdlib sqlite3 lib with the pysqlite3 
-package__import__('pysqlite3')
+__import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
