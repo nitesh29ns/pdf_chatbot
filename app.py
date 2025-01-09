7d69c256cbc8d2e5c85b3f519bc74eec5f41d2a7
Nitesh Sharma <niteshsharma29.ns@gmail.com>
2025-01-09 19:49:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-# these three lines swap the stdlib sqlite3 lib with the pysqlite3 
-package__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 import os
 import streamlit as st
 from chatbot import ChatBot_output
