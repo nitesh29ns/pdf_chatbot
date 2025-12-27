892787cece417938f1e702f32db99b1fabc9705c
Nitesh Sharma <niteshsharma29.ns@gmail.com>
2025-12-27 18:26:17 +0530

    Update data_loading.py

--- data_loading.py
+++ data_loading.py
@@ -3,7 +3,7 @@ import os
   # os.environ["NOMIC_API_KEY"] = "nk-2qWDlRSfhpW2XxhDackPfJxBMJwVfFoxx_Hy-ae5dxE"
 import streamlit as st
 if "NOMIC_API_KEY" in st.secrets:
-    os.environ["NOMIC_API_KEY"] = st.secrets["NOMIC_API_KEY "]
+    os.environ["NOMIC_API_KEY"] = st.secrets["NOMIC_API_KEY"]
 from langchain_nomic import NomicEmbeddings  
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
